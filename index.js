9d6d1135bdc8fb16ec169c80d9f82b67e3626027
anvar mamarajabov <anvar.dev1909@gmail.com>
2025-08-26 13:33:40 +0500

    TOken

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 
-import getArgs from './helpers/args.js';
-import { printError, printSuccess, printHelp } from './services/log.service.js';
+import getArgs from './helpers/args.js'
+import { printError, printHelp, printSuccess } from './services/log.service.js'
+import { saveKeyValue } from './services/storage.service.js'
+
+const saveToken = async(token)=>{
+	try {
+		await saveKeyValue('token', token)
+		printSuccess('Token saved')
+		
+	} catch (error) {
+		return printError(error.message)
+	}
+}
 const startCli =()=>{
 	const args = getArgs(process.argv);
-	printSuccess('ok')
-	printError('error')
+	// printSuccess('ok')
+	// printError('error')
 	console.log(args);
 
 	if(args.h){
@@ -19,6 +30,7 @@ const startCli =()=>{
 	}
 	if(args.t){
 		// token
+		return saveToken(args.t)
 	}
 	// result
 }
